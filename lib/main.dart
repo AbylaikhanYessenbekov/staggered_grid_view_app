f7aac2eb70fa3a43ede7f72c8925217966304578
abylaikhan <akyessenbekov@gmail.com>
2020-11-17 09:24:10 +0500

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+
         appBar: AppBar(
           title: Text('StaggeredGridView'),
         ),
